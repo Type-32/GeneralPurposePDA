32e34c17a8448334e8d2ef94303d143b029188e9
Type-32 <87076491+Type-32@users.noreply.github.com>
2024-09-23 08:37:42 +0800

    feat: init repo

---
+++ main.py
@@ -0,0 +1,21 @@
+import time
+
+import esp
+import network
+
+def main():
+    print("ello")
+    connect_wifi('SIA', 'your_password')
+
+def connect_wifi(ssid, password):
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('Connecting to WiFi...')
+        wlan.connect(ssid, password)
+        while not wlan.isconnected():
+            time.sleep(1)
+    print('WiFi connected:', wlan.ifconfig())
+
+if __name__ == '__main__':
+    main()
